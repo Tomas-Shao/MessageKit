2f3a8b45682dddfe66b882bf225dfb9261bf16b9
tomas.shao <zeliang.shao@gmail.com>
2022-04-13 19:08:39 +0800

    change text

--- Sources/Protocols/CallItem.swift
+++ Sources/Protocols/CallItem.swift
@@ -8,4 +8,28 @@ public protocol CallItem {
 
     /// The status text of the call (e.g., "Missed Call", "Call Duration: 2:30").
     var statusText: String { get }
+
+    /// The url where the audio file is located.
+    var hasVideo: Bool { get }
+
+    /// The call duration in seconds.
+    var duration: Int { get }
+
+    var title: String { get }
+}
+
+extension CallItem {
+
+    var displayText: String {
+        duration <= 0 ? title : title + " " + secondsToHoursMinutesSeconds(duration)
+    }
+
+    private func secondsToHoursMinutesSeconds(_ seconds: Int) -> String {
+        let value =  (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+        if value.0 > 0 {
+            return String(format: "%02i:%02i:%02i", value.0, value.1, value.2)
+        } else {
+            return String(format: "%02i:%02i", value.1, value.2)
+        }
+    }
 }
